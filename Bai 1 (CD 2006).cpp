bd2b69d370a72ddf5b605a1ce9cbba8f8a95a79a
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2019-08-05 12:07:22 +0700

    Bài 1 Đề CD 2006 Sửa lần 2 (05/08/2019)

--- Bai 1 (CD 2006).cpp
+++ Bai 1 (CD 2006).cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
@@ -13,9 +13,27 @@ int countOne(string s) {
 	return count(s.begin(), s.end(), '1');
 }
 
+bool checkChuoiCon(string s1, string s2) {
+	// s1 se la con cua s2
+	int i = 0; // index cua s1
+	int j = 0; // index cua s2
+	int count = 0; // kiem tra da duyet qua het i
+
+	while (i < s1.size() && j < s2.size()) {
+		if (s1[i] == s2[j]) {
+			i++;
+			j++;
+			count++;
+		} else {
+			j++;
+		}
+	}
+
+	return count == s1.size(); 
+}
+
 int main() {
 	ifstream docFile;
-	ofstream ghiFile;
 
 	// Doc so luong sieu ma
 	docFile.open("HCODE.INP");
@@ -30,40 +48,40 @@ int main() {
 	for (int i = 0; i < iN; i++) {
 		getline(docFile, sieuMa[i]);
 	}
+	docFile.close();
 
 	for (int i = 0; i < iN; i++) {
 		cout << sieuMa[i] << " Zero: " << countZero(sieuMa[i]) << " One: " << countOne(sieuMa[i]) << endl;
 	}
 
-	int count = 0;
-	int min = iN;
-	for (int i = 0; i < iN - 1; i++) {
+	int min = iN; // Chua vi tri nho nhat
+	bool check = false; // Kiem tra su ton tai cua chuoi con
+
+	for (int i = 0; i < iN ; i++) {
 		for (int j = i + 1; j < iN; j++) {
+			// Kiem tra chuoi nao la chuoi con
+			// Roi kiem tra su ton tai cua chuoi con trong chuoi cha theo thu tu tang dan
 			if (sieuMa[i].size() > sieuMa[j].size()) {
-				if  (countZero(sieuMa[j]) <= countZero(sieuMa[i]) && countOne(sieuMa[j]) <= countOne(sieuMa[i])) {
-					count++;
-					if (min > i) {
-						min = i;
-					}
-				} else {
-					break;
+				if (checkChuoiCon(sieuMa[j], sieuMa[i])) {
+					check = true;
+					i > j ? min = j : min = i;
+					goto ghiFile; // Chi can co 1 chuoi con thi thoat vong lap
 				}
-				// countZero(sieuMa[j]) <= countZero(sieuMa[i]) && countOne(sieuMa[j]) <= countOne(sieuMa[i])
 			} else {
-				if (countZero(sieuMa[i]) <= countZero(sieuMa[j]) && countOne(sieuMa[i]) <= countOne(sieuMa[j])) {
-					count++;
-					if (min > j) {
-						min = j;
-					}
-				} else {
-					break;
+				if (checkChuoiCon(sieuMa[i], sieuMa[j])) {
+					check = true;
+					cout << i << " " << j << endl;
+					i > j ? min = j : min = i;
+					goto ghiFile;
 				}
 			}
 		}
 	}
 
+ghiFile:
+	ofstream ghiFile;
 	ghiFile.open("HCODE.OUT");
-	count == iN ? ghiFile << 1 : ghiFile << 0 << endl << min + 1;
+	check ? ghiFile << 0 << endl << min + 1 : ghiFile << 1;
 
 	system("pause");
 	return 0;
