7182e42556ba7d25b75c38c50523bdeab0948cfc
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2019-08-05 13:36:45 +0700

    Bài 2 Đề CD 2006 (05/08/2019)

---
+++ Bai 2 (CD 2006).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+// Tinh tong cot doc
+int sumRow(vector<vector<int>> arr, int y, int x) {
+	int sum = 0;
+	for (int i = 0; i <= y; i++) {
+		sum += arr[i][x];
+	}	
+	return sum;
+}
+
+int main() {
+	ifstream docFile;
+	int x, y = 0;
+	
+	// Doc gia tri cot doc va ngang
+	docFile.open("IIMAGE.INP");
+	docFile >> y; // Doc
+	docFile >> x; // Ngang
+
+	ofstream ghiFile;
+	ghiFile.open("IIMAGE.OUT");
+
+	vector<vector<int>> image(y, vector<int>(x));
+
+	int prev; // luu ket qua truoc do
+
+	for (int i = 0; i < y; i++) {
+		prev = 0; // Tro ve 0 sau moi dong ket thuc
+		for (int j = 0; j < x; j++) {
+			docFile >> image[i][j];
+			if (j == 0) {
+				ghiFile << sumRow(image, i, j) + prev;
+				prev = sumRow(image, i, j);
+			} else {
+				ghiFile << sumRow(image, i, j) + prev;
+				prev = sumRow(image, i, j) + prev;
+			}
+			if (j != x - 1) {
+				ghiFile << " "; // Neu khong phai so cuoi cung thi cach ra
+			}
+		}
+		ghiFile << endl;
+	}
+
+	docFile.close();
+	ghiFile.close();
+
+	system("pause");
+	return 0;
+}
